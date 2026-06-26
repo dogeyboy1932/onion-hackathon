e0d794a3c879ba4c7fc2206770d3a5da08bfc09a
RJ Catalano <richard@thrackle.io>
2026-06-25 19:29:17 -0500

    state added
    Signed-off-by: RJ Catalano <richard@thrackle.io>

--- programs/hackathon/src/state.rs
+++ programs/hackathon/src/state.rs
@@ -1,6 +1,46 @@
 
 
+
+enum DragonMood {
+    Angry,
+    Sad,
+    Happy
+}
+
+enum Status {
+    Available,
+    Assigned,
+    InFlight,
+    Resting,
+    Injured,
+    Retired,
+}
+
+enum MissionStatus {
+    Pending,
+    InProgress,
+    Completed,
+    Failed
+}
+
 struct Dragon {
-    name: str,
+    name: String,
     fire_level: u16,
+    mood: DragonMood,
+    status: Status,
+    damage_recorded: u64,
+    created_at: i64,
+}
+
+struct Mission {
+    dragon: Dragon,
+    target: Village,
+    risk_level: u16,
+    status: MissionStatus,
+}
+
+struct Village {
+    name: String,
+    damage: u64,
+    incidents: Vec<String>,
 }
