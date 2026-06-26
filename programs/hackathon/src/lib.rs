518d4ad476dcf2edd9652c1d1bd99f255c716453
RJ Catalano <richard@thrackle.io>
2026-06-25 20:06:31 -0500

    try to consolidate
    Signed-off-by: RJ Catalano <richard@thrackle.io>

--- programs/hackathon/src/lib.rs
+++ programs/hackathon/src/lib.rs
@@ -5,6 +5,7 @@ pub mod state;
 
 use anchor_lang::prelude::*;
 
+
 pub use constants::*;
 pub use instructions::*;
 
@@ -12,9 +13,49 @@ declare_id!("F8dWQZHCnRQB7TjGebxzQTTkZbsjSmBtysLsvbgFgdhx");
 
 #[program]
 pub mod hackathon {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    enum DragonMood {
+        Angry,
+        Sad,
+        Happy
     }
+
+    enum Status {
+        Available,
+        Assigned,
+        InFlight,
+        Resting,
+        Injured,
+        Retired,
+    }
+
+    enum MissionStatus {
+        Pending,
+        InProgress,
+        Completed,
+        Failed
+    }
+
+    pub struct Dragon {
+        name: String,
+        fire_level: u16,
+        mood: DragonMood,
+        status: Status,
+        damage_recorded: u64,
+        created_at: i64,
+    }
+
+    pub struct Mission {
+        dragon: Dragon,
+        target: Village,
+        risk_level: u16,
+        status: MissionStatus,
+    }
+
+    pub struct Village {
+        name: String,
+        damage: u64,
+        incidents: Vec<String>,
+    }
+
+   
 }
