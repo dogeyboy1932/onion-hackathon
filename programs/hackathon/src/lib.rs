f66ee568d19eea99259d38d74e8a3eadda3e3514
RJ Catalano <richard@thrackle.io>
2026-06-25 20:11:43 -0500

    add in intermediate stage that we can iterate from there
    Signed-off-by: RJ Catalano <richard@thrackle.io>

--- programs/hackathon/src/lib.rs
+++ programs/hackathon/src/lib.rs
@@ -13,6 +13,7 @@ declare_id!("F8dWQZHCnRQB7TjGebxzQTTkZbsjSmBtysLsvbgFgdhx");
 
 #[program]
 pub mod hackathon {
+    use super::*;
     enum DragonMood {
         Angry,
         Sad,
@@ -56,6 +57,67 @@ pub mod hackathon {
         damage: u64,
         incidents: Vec<String>,
     }
+    
+    pub fn close(_ctx: Context<CloseCounter>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Update>) -> Result<()> {
+        ctx.accounts.counter.count = ctx.accounts.counter.count.checked_sub(1).unwrap();
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Update>) -> Result<()> {
+        ctx.accounts.counter.count = ctx.accounts.counter.count.checked_add(1).unwrap();
+        Ok(())
+    }
+
+    pub fn initialize(_ctx: Context<InitializeCounter>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
+        ctx.accounts.counter.count = value.clone();
+        Ok(())
+    }
+    
+    #[derive(Accounts)]
+    pub struct InitializeCounter<'info> {
+        #[account(mut)]
+        pub payer: Signer<'info>,
+    
+        #[account(
+      init,
+      space = 8 + Counter::INIT_SPACE,
+      payer = payer
+        )]
+        pub counter: Account<'info, Counter>,
+        pub system_program: Program<'info, System>,
+    }
+    #[derive(Accounts)]
+    pub struct CloseCounter<'info> {
+        #[account(mut)]
+        pub payer: Signer<'info>,
+    
+        #[account(
+      mut,
+      close = payer, // close account and return lamports to payer
+        )]
+        pub counter: Account<'info, Counter>,
+    }
+    
+    #[derive(Accounts)]
+    pub struct Update<'info> {
+        #[account(mut)]
+        pub counter: Account<'info, Counter>,
+    }
+    
+    #[account]
+    #[derive(InitSpace)]
+    pub struct Counter {
+        count: u8,
+    }
+}
 
    
-}
+
